cb9ae21c343c9ac491184b4fe11a1140093ff1ca
CharlesGabo <charles.martinez232610@gmail.com>
2026-03-25 19:36:51 +0800

    profile page done

--- assets/js/capstone_db.sql
+++ assets/js/capstone_db.sql
@@ -951,6 +951,7 @@ CREATE TABLE `users` (
   `employee_number` varchar(20) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(30) DEFAULT NULL,
+  `profile_photo` varchar(500) DEFAULT NULL,
   `password_hash` varchar(255) NOT NULL,
   `account_type` varchar(20) NOT NULL DEFAULT 'student',
   `has_unpaid_debt` tinyint(1) NOT NULL DEFAULT 0,
@@ -964,15 +965,15 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `student_number`, `program_id`, `institute_id`, `first_name`, `last_name`, `employee_number`, `email`, `phone`, `password_hash`, `account_type`, `has_unpaid_debt`, `is_active`, `last_login_at`, `created_at`, `updated_at`) VALUES
-(1, NULL, NULL, NULL, 'osa', '-', 'osa', 'osa@gmail.com', '+63 1234567890', '$2y$10$84lLDKnVilseyCYVSCFrxOherPdoFcnV.2/I261teTT6mPKasRCOS', 'osa_staff', 0, 1, '2026-03-25 00:15:46', '2026-02-25 18:19:21', '2026-03-25 00:15:46'),
-(4, 'aisers', 2, 1, 'aisers', 'aisers', NULL, 'aisers@gmail.com', '+63 1234567890', '$2y$10$0FvPli/TnKLKOkmyei3lBOK.S1VBZWA/.SDI2Zlr1SoT6miFT2bIK', 'student', 0, 1, '2026-03-24 23:07:57', '2026-02-27 11:44:45', '2026-03-24 23:07:57'),
-(6, '12324MN-000094', 2, 1, 'Charles Gabriel A.', 'Martinez', NULL, 'charles.martinez232610@gmail.com', '+63 9763395956', '$2y$10$R4Sx52NV6nncQbl3mw7ctuqYaC2jdlv9IIWKB1/w5fa56vfF5A49a', 'student', 0, 1, '2026-02-27 17:32:33', '2026-02-27 13:48:26', '2026-02-27 17:32:33'),
-(7, 'ssc', 2, 1, 'ssc', 'ssc', NULL, 'ssc@gmail.com', '+63 1234567890', '$2y$10$UM5ah0sZDl.eUF1Y7MWuxONTCIXd6ZHpTGy8WqYX6aEGu9fxl/LVe', 'student', 0, 1, '2026-03-24 21:15:10', '2026-02-28 19:45:28', '2026-03-24 21:15:10'),
-(8, 'ISstudent', 2, 1, 'ISstudent', 'ISstudent', NULL, 'ISstudent@gmail.com', '+63 1234567890', '$2y$10$APOjL8Lk48.8uirVzSUyfenEHQJ5OGcey/5lOOUnUwKjOKiPebUZu', 'student', 0, 1, '2026-03-24 23:08:01', '2026-03-18 21:01:34', '2026-03-24 23:08:01'),
-(9, 'ITstudent', 1, 1, 'ITstudent', 'ITstudent', NULL, 'ITstudent@gmail.com', '+63 1234567890', '$2y$10$RH8GmvgHTHJGS20BD4x69O2HSV/33cQtSfzmm62j6jRJXupt2/I9O', 'student', 0, 1, '2026-03-24 23:34:04', '2026-03-18 21:05:46', '2026-03-24 23:34:04'),
-(10, 'AETstudent', 3, 2, 'AETstudent', 'AETstudent', NULL, 'AETstudent@gmail.com', '+63 1234567890', '$2y$10$Pf0QzbAfnqQBMTUy1nn/4.xkxO7AeVIhxB.OsBQWR45lV4CXrLjI6', 'student', 0, 1, '2026-03-24 23:37:35', '2026-03-18 21:20:54', '2026-03-24 23:37:35'),
-(11, 'elitech', 1, 1, 'elitech', 'elitech', NULL, 'elitech@gmail.com', '+63 1234567890', '$2y$10$IYHhWOTf2dIs6FfOiVX99.iDQ8sRYvRi4HGtEuOWg/tuS0vPC0cw2', 'student', 1, 1, '2026-03-24 23:33:54', '2026-03-18 21:26:31', '2026-03-24 23:33:54');
+INSERT INTO `users` (`user_id`, `student_number`, `program_id`, `institute_id`, `first_name`, `last_name`, `employee_number`, `email`, `phone`, `profile_photo`, `password_hash`, `account_type`, `has_unpaid_debt`, `is_active`, `last_login_at`, `created_at`, `updated_at`) VALUES
+(1, NULL, NULL, NULL, 'osa', '-', 'osa', 'osa@gmail.com', '+63 1234567890', NULL, '$2y$10$84lLDKnVilseyCYVSCFrxOherPdoFcnV.2/I261teTT6mPKasRCOS', 'osa_staff', 0, 1, '2026-03-25 00:15:46', '2026-02-25 18:19:21', '2026-03-25 00:15:46'),
+(4, 'aisers', 2, 1, 'aisers', 'aisers', NULL, 'aisers@gmail.com', '+63 1234567890', NULL, '$2y$10$0FvPli/TnKLKOkmyei3lBOK.S1VBZWA/.SDI2Zlr1SoT6miFT2bIK', 'student', 0, 1, '2026-03-24 23:07:57', '2026-02-27 11:44:45', '2026-03-24 23:07:57'),
+(6, '12324MN-000094', 2, 1, 'Charles Gabriel A.', 'Martinez', NULL, 'charles.martinez232610@gmail.com', '+63 9763395956', NULL, '$2y$10$R4Sx52NV6nncQbl3mw7ctuqYaC2jdlv9IIWKB1/w5fa56vfF5A49a', 'student', 0, 1, '2026-02-27 17:32:33', '2026-02-27 13:48:26', '2026-02-27 17:32:33'),
+(7, 'ssc', 2, 1, 'ssc', 'ssc', NULL, 'ssc@gmail.com', '+63 1234567890', NULL, '$2y$10$UM5ah0sZDl.eUF1Y7MWuxONTCIXd6ZHpTGy8WqYX6aEGu9fxl/LVe', 'student', 0, 1, '2026-03-24 21:15:10', '2026-02-28 19:45:28', '2026-03-24 21:15:10'),
+(8, 'ISstudent', 2, 1, 'ISstudent', 'ISstudent', NULL, 'ISstudent@gmail.com', '+63 1234567890', NULL, '$2y$10$APOjL8Lk48.8uirVzSUyfenEHQJ5OGcey/5lOOUnUwKjOKiPebUZu', 'student', 0, 1, '2026-03-24 23:08:01', '2026-03-18 21:01:34', '2026-03-24 23:08:01'),
+(9, 'ITstudent', 1, 1, 'ITstudent', 'ITstudent', NULL, 'ITstudent@gmail.com', '+63 1234567890', NULL, '$2y$10$RH8GmvgHTHJGS20BD4x69O2HSV/33cQtSfzmm62j6jRJXupt2/I9O', 'student', 0, 1, '2026-03-24 23:34:04', '2026-03-18 21:05:46', '2026-03-24 23:34:04'),
+(10, 'AETstudent', 3, 2, 'AETstudent', 'AETstudent', NULL, 'AETstudent@gmail.com', '+63 1234567890', NULL, '$2y$10$Pf0QzbAfnqQBMTUy1nn/4.xkxO7AeVIhxB.OsBQWR45lV4CXrLjI6', 'student', 0, 1, '2026-03-24 23:37:35', '2026-03-18 21:20:54', '2026-03-24 23:37:35'),
+(11, 'elitech', 1, 1, 'elitech', 'elitech', NULL, 'elitech@gmail.com', '+63 1234567890', NULL, '$2y$10$IYHhWOTf2dIs6FfOiVX99.iDQ8sRYvRi4HGtEuOWg/tuS0vPC0cw2', 'student', 1, 1, '2026-03-24 23:33:54', '2026-03-18 21:26:31', '2026-03-24 23:33:54');
 
 --
 -- Triggers `users`
